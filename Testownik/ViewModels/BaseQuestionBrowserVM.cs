a5aaa546a8bedf40eebc36d76681fa9ee87b06d9
Nuzumi <michal.sewerniak@o2.pl>
2017-08-04 12:29:39 +0200

    dodano aktualna wartosc sliderna na ekranie glownym i contextMnu do 2 przyciskow w trybie edycji

--- Testownik/ViewModels/BaseQuestionBrowserVM.cs
+++ Testownik/ViewModels/BaseQuestionBrowserVM.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Testownik.ViewModels
@@ -50,5 +51,6 @@ namespace Testownik.ViewModels
             addEdit.Show();
             window.Close();
         }
+
     }
 }
